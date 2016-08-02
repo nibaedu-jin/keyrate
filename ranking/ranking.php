aaa7450d8f158f2d8cb0d4a133f8a7ca9d7e6e33
yitongbot <jarvis.yt@gmail.com>
2016-08-02 22:55:03 +0800

    update dbconfig

--- ranking/ranking.php
+++ ranking/ranking.php
@@ -49,14 +49,14 @@
       <center>
         <div id="grad1"><h1> 打 字 排 名 </h1></div>
 
-        <h2>目前张三获得第一名</h2>
+        <h2>目前<?PHP //TODO 动态第一名?>获得第一名</h2>
       </center>
 
       <form action="index1.php" method="post">
         用户名: <input type="text" name="name" />
         <input type="submit" />
       </form>
-      <p>  
+      <p>
       </p>
       <div align="center" style="border:4px solid red">
         <table border="8" cellspacing="10px" cellspacing="10px">
@@ -66,13 +66,12 @@
           <tr>
             <th style="width:100px; "><h3>名次</h3></th>
             <th style="width:150px; "><h3>用户名</h3></th>
-            <th style="width:230px; "><h3>文章</h3></th>
-            <th style="width:130px; "><h3>类型</h3></th>
-            <th style="width:130px; "><h3>时长</h3></th>
             <th style="width:130px; "><h3>成绩</h3></th>
             <th style="width:240px; "><h3>时间</h3></th>
           </tr>
-
+          <?php
+          //TODO 列表
+          ?>
 
           <tr>
             <th>1</th>
@@ -83,83 +82,11 @@
             <th>199KPM</th>
             <th>2016.7.14-15:24:53</th>
           </tr>
-          <tr>
-            <th>2</th>
-            <th>李四</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>170KPM</th>
-            <th>2016.7.14-15:24:43</th>
-          </tr>
 
-          <tr>
-            <th>3</th>
-            <th>王二</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>145KPM</th>
-            <th>2016.7.14-15:24:2</th>
-          </tr>
-
-          <tr>
-            <th>4</th>
-            <th>麻子</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>130KPM</th>
-            <th>2016.7.14-15:24:26</th>
-          </tr>
-
-          <tr>
-            <th>5</th>
-            <th>欢欢</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>120KPM</th>
-            <th>2016.7.14-15:24:33</th>
-          </tr>
-
-          <tr>
-            <th>6</th>
-            <th>糖糖</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>119KPM</th>
-            <th>2016.7.14-15:24:53</th>
-          </tr>
-
-          <tr>
-            <th>7</th>
-            <th>冰冰</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>110KPM</th>
-            <th>2016.7.14-15:23:24</th>
-          </tr>
-
-          <tr>
-            <th>8</th>
-            <th>宁宁</th>
-            <th>a girl selling matches</th>
-            <th>English</th>
-            <th>5分钟</th>
-            <th>99KPM</th>
-            <th>2016.7.14-15:25:29</th>
-          </tr>
 
         </table>
-        <p>
-        </p>
-        <p>
-        </p>
-        <button>下一页</button>
-        <a href="http://192.168.1.102/dazijilu.html" class="button">继续练习</a>
+
+        <a href="javascript:void(0);" onclick="window.open('http://dazi.kukuw.com/');"class="button">继续练习</a>
 
       </body>
 
