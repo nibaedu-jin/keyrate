05e10a333cf4b9dcc6c7f2a2ed6f658a79dc9a63
yitongbot <jarvis.yt@gmail.com>
2016-08-02 10:00:22 +0800

    fix bug

--- login/loginmanage.php
+++ login/loginmanage.php
@@ -1,7 +1,7 @@
 <?php
 //给浏览器的HTML头 设置为utf-8格式
 header('Content-type:text/html;charset=utf-8');
-//数据库连接 使用pdo方式
+//数据库连接 使用dbo方式
 include "../db/config.php";
 //新建变量，并将指赋为在Post中拿到的指
 $username = $_POST['username'];
@@ -15,7 +15,7 @@ $result = $db->query($sql) or die('数据库连接失败，错误信息：');
 if($result){
   echo $sql;
     //登录成功
-    header("Location: ../ranking/ranking.php");
+    header("Location: ../update/update.php");
     exit;
 } else {
     echo ('登录失败！点击此处 <a href="javascript:history.back(-1);">返回</a> 重试');
