691be19bdb170c82dfb8c12a97cb656822a328cd
Doublemore <495936483@qq.com>
2016-08-02 16:30:57 +0800

    update share

--- db/config.php
+++ db/config.php
@@ -1,8 +1,6 @@
 <?php
 // 数据库链接相关信息
-$dbhost="192.168.1.132";
+$dbhost="127.0.0.1";
 $dbname="keyrate";
 $dbuser="root";
-$dbpwd="pwd";
-// 链接字符串
-$db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';charset=utf8', $dbuser, $dbpwd);
+$dbpwd="amal";
